c9f1d3ea9ea48f5b63bdd351a2f87399a6d60ed3
ManiMindScripts <ar7279796@gmail.com>
2024-09-04 14:27:10 -0700

    Header Section Cmp

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,40 +1,41 @@
-import React ,{useState} from 'react'
+import React, { useState } from 'react'
 import fanta1 from '../assets/fanta1.png'
 import fanta2 from '../assets/fanta2.png'
 import fanta3 from '../assets/fanta3.png'
 import { FaWhatsapp } from 'react-icons/fa'
 import Navbar from './Navbar'
-import { motion,AnimatePresence,easeInOut,} from 'framer-motion'
+import { motion, AnimatePresence, easeInOut, animate, } from 'framer-motion'
+import { UpdateFollower } from 'react-mouse-follower'
 
 const sideRight = (delay) => {
-     return{
-      hidden: {
-           opacity: 0,
-           x:100,
-      },
-     show: {
-          opacity:1,
-          x:0,
-      transition:{
-        duration:0.5,
+  return {
+    hidden: {
+      opacity: 0,
+      x: 100,
+    },
+    show: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
         delay: delay,
         ease: easeInOut,
       },
     },
-     exit:{
-      opacity:0,
-      x: -50, 
-      transition:{
-        duration:0.2,
+    exit: {
+      opacity: 0,
+      x: -50,
+      transition: {
+        duration: 0.2,
         ease: easeInOut,
       },
 
-     }
-      
-     }
-    
-
     }
+
+  }
+
+
+}
 
 
 const HeroData = [
@@ -71,91 +72,170 @@ const HeroData = [
 const Hero = () => {
   const [activeData, setactiveData] = useState(HeroData[0])
 
-  const handActiveData =(data) => {
+  const handActiveData = (data) => {
     setactiveData(data)
   }
   return (
     <>
-    <motion.div  initial = {{backgroundColor: activeData.bgColor}}
-    animate = {{backgroundColor: activeData.bgColor}}
-    transition={{duration: 0.8}}
-    >
-     {/* NAvbar Component */}
-     <Navbar/>
-     <div className='container grid grid-cols-1 md:grid-cols-2 min-h-[500px]'>
-     {/* Data Info */}
-       <div className='justify-center flex flex-col py-14 md:py-0 xl:max-w-[500px] text-white order-2 md:order-1'>
-        <div className='space-y-5 text-center md:text-left'>
+      <motion.div initial={{ backgroundColor: activeData.bgColor }}
+        animate={{ backgroundColor: activeData.bgColor }}
+        transition={{ duration: 0.8 }}
+      >
+        {/* NAvbar Component */}
+        <Navbar />
+        <div className='container grid grid-cols-1 md:grid-cols-2 min-h-[500px]'>
+          {/* Data Info */}
+          <div className='justify-center flex flex-col py-14 md:py-0 xl:max-w-[500px] text-white order-2 md:order-1 relative z-40'>
+            <div className='space-y-5 text-center md:text-left'>
+              <AnimatePresence mode='wait'>
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: "white",
+                    zIndex: 10,
+                    followSpeed: 0.5,
+                    scale: 10,
+                    mixBlendMode: "difference"
+                  }}
+                >
+                  <motion.h1
+                    key={activeData.id}
+                    variants={sideRight(0.2)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    className='text-3xl lg:text-6xl xl:text-7xl font-bold font-handwriting text-shadow'> {activeData.title}
+                  </motion.h1>
+                </UpdateFollower>
+              </AnimatePresence>
+              <AnimatePresence mode='wait'>
+                <motion.p
+                  key={activeData.id}
+                  variants={sideRight(0.4)}
+                  initial="hidden"
+                  animate="show"
+                  exit="exit"
+                  className='text-sm leading-lose text-white/80'> {activeData.subtitle}
+                </motion.p>
+              </AnimatePresence>
+              <AnimatePresence mode='wait'>
+                <UpdateFollower
+                  mouseOptions={{
+                    backgroundColor: activeData.bgColor,
+                    zIndex: 9999,
+                    followSpeed: 0.5,
+                    rotate: -720,
+                    scale: 6,
+                    backgroundElement: (
+                      <div>
+                        <img src={activeData.image} />
+                      </div>
+                    ),
+                  }}
+                >
+                  <motion.button
+                    key={activeData.id}
+                    variants={sideRight(0.6)}
+                    initial="hidden"
+                    animate="show"
+                    exit="exit"
+                    style={{
+                      color: activeData.bgColor
+                    }}
+                    className='px-4 py-2 bg-white inline-block font-normal rounded-sm font-handwriting text-black'>Order Now
+                  </motion.button>
+                </UpdateFollower>
+              </AnimatePresence>
+              {/* Seprator */}
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.2, ease: easeInOut }}
+                className='text-center flex justify-center md:justify-start gap-4 !md:mt-24 !mb-10'>
+                <div className='w-20 h-[1px] bg-white'></div>
+                <p>TOP RECOMENDATION</p>
+                <div className='w-20 h-[1px] bg-white'></div>
+              </motion.div>
+              {/* Image Swiper */}
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5, delay: 0.2, ease: easeInOut }}
+                className='grid grid-cols-3 gap-10'>
+                {
+                  HeroData.map((data) => {
+                    return (
+                      <UpdateFollower
+                        mouseOptions={{
+                          backgroundColor: data.bgColor,
+                          zIndex: 9999,
+                          followSpeed: 0.5,
+                          scale: 5,
+                          text: "View Details",
+                          textFontSize: "3px",
+                        }}
+                      >
+                        <div onClick={() => handActiveData(data)}
 
-      <motion.h1 
-      key ={activeData.id}
-      variants={sideRight(0.2)}
-      initial= "hidden"
-      animate = "show"
-      exit = "exit"
-      className='text-3xl lg:text-6xl xl:text-7xl font-bold font-handwriting text-shadow'> {activeData.title}
-      </motion.h1>
-      <motion.p 
-       key ={activeData.id}
-      variants={sideRight(0.4)}
-      initial= "hidden"
-      animate = "show"
-      exit = "exit"
-      
-      className='text-sm leading-lose text-white/80'> {activeData.subtitle}</motion.p>
-      <motion.button
-       key ={activeData.id}
-      variants={sideRight(0.6)}
-      initial= "hidden"
-      animate = "show"
-      exit = "exit"
-       className='px-4 py-2 bg-white inline-block font-normal rounded-sm text-black'>Order Now 
-      </motion.button>
-      {/* Seprator */}
-      <motion.div
-      
-       className='text-center flex justify-center md:justify-start gap-4 !md:mt-24 !mb-10'>
-        <div className='w-20 h-[1px] bg-white'></div>
-        <p>TOP RECOMENDATION</p>
-          <div className='w-20 h-[1px] bg-white'></div>  
-      </motion.div>
-      {/* Image Swiper */}
-      <div className='grid grid-cols-3 gap-10'>
-      {
-        HeroData.map((data)=> {
-          return (
-            <div className='cursor-pointer space-y-3 hover:scale-105 transition-all duration-200'>
-            <div className='justify-center flex'>
-              <img src={data.image} alt='' className={
-                `img-shadow w[80px] ${activeData.image === data.image? "opacity-100 scale-100" : "opacity-50" }`
-              }/>
+                          className='cursor-pointer space-y-3 hover:scale-105 transition-all duration-200'>
+                          <div className='justify-center flex'>
+                            <img src={data.image} alt='' className={
+                              `img-shadow w[80px] ${activeData.image === data.image ? "opacity-100 scale-100" : "opacity-50"}`
+                            } />
+                          </div>
+                          <div className='!mt-6 text-center space-y-1'>
+                            <p className='text-base opacity-50 line-through'>{data.price}</p>
+                            <p className='text-xl font-bold'>{data.price}</p>
+                          </div>
+                        </div>
+                      </UpdateFollower>
+                    )
+                  })
+                }
+              </motion.div>
             </div>
-            <div className='!mt-6 text-center space-y-1'>
-              <p className='text-base opacity-50 line-through'>{data.price}</p>
-              <p className='text-xl font-bold'>{data.price}</p>
-            </div>
-            </div>
-          )
-        })
-      }
-      </div>
+          </div>
+          {/* Hero Img */}
+          <div className='justify-end flex flex-col items-center relative order-1 md:order-2'>
+            <AnimatePresence mode='wait'>
+              <motion.img
+                key={activeData.id}
+                initial={{ opacity: 0, x: 100 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.4, delay: 0, ease: easeInOut }}
+                exit={{
+                  opacity: 0,
+                  x: -100,
+                  transition: {
+                    duration: 0.4
+                  },
+                }}
+                src={activeData.image} alt='' className='w-[150px] md:w-[200px] xl:w-[250px] img-shadow relative z-10' />
+            </AnimatePresence>
+            <AnimatePresence mode='wait'>
+              <motion.div
+                key={activeData.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.4, delay: 0.0, ease: easeInOut }}
+                exit={{
+                  opacity: 0,
+                  transition: {
+                    duration: 0.4
+                  },
+                }}
+                className='text-white/5 text-[300px] font-poppins font-extrabold absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+                {activeData.modal}
+              </motion.div>
+            </AnimatePresence>
+          </div>
+          {/* Whatsapp Icon */}
+          <div className='text-3xl text-white fixed bottom-10 right-10 hover:rotate-[360deg] duration-500 z-[99999] mix-blend-difference'>
+            <a href='#'>
+              <FaWhatsapp />
+            </a>
+          </div>
         </div>
-       </div>
-     {/* Hero Img */}
-     <div className='justify-end flex flex-col items-center relative order-1 md:order-2'>
-      <img src={activeData.image} alt='' className='w-[150px] md:w-[200px] xl:w-[250px] img-shadow relative z-10'/>
-      <div className='text-white/5 text-[300px] font-poppins font-extrabold absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        {activeData.modal}
-      </div>
-     </div>
-     {/* Whatsapp Icon */}
-      <div className='text-3xl text-white fixed bottom-10 hover:rotate-[360deg] duration-500 z-[99999] mix-blend-difference'>
-        <a href='#'>
-        <FaWhatsapp/>
-        </a>
-      </div>
-     </div>
-     </motion.div>
+      </motion.div>
 
     </>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import img from "../assets/logo.png"
 import { MdMenu } from "react-icons/md";
 import { FaRegUser } from 'react-icons/fa';
+import { motion } from 'framer-motion';
+import { UpdateFollower } from 'react-mouse-follower';
 
 
 const NavbarMenu = [
@@ -9,59 +11,83 @@ const NavbarMenu = [
     id: 1,
     title: "Home",
     link: "#"
-},
-{
-  id: 2,
-  title: "Categories",
-  link: "#"
-},
-{
-  id: 3,
-  title: "Blog",
-  link: "#"
-},
-{
-  id: 4,
-  title: "About",
-  link: "#"
-},
-{
-  id: 5,
-  title: "Contact",
-  link: "#"
-},
+  },
+  {
+    id: 2,
+    title: "Categories",
+    link: "#"
+  },
+  {
+    id: 3,
+    title: "Blog",
+    link: "#"
+  },
+  {
+    id: 4,
+    title: "About",
+    link: "#"
+  },
+  {
+    id: 5,
+    title: "Contact",
+    link: "#"
+  },
 ]
 
 const Navbar = () => {
   return (
     <>
-    <div className= 'text-white py-8'>
-    <div className='container flex justify-between items-center'>
-    <div>
-       <img src={img} alt='' className='max-w-[100px] invert'/>
-       </div>
-       {/* Menu Section */}
-       <div className='hidden md:block'>
-        <ul className='flex items-center relative gap-4 z-40'>
-        {
-          NavbarMenu.map((item)=> {
-           return <li>
-              <a href={item.link} className='text-base inline-block font-semibold py-2 px-3 uppercase'>{item.title}</a>
-            </li>
-          })}
-          <button className='ps-14 text-xl'>
-            <FaRegUser/>
-          </button>
-        </ul>
-       </div>
-        {/* Menu Section */}
-        {/* Humbermenu Icon */}
-        <div className='md:hidden'>
-             <MdMenu className='text-4xl'/>
-        </div>
-        {/* Humbermenu Icon */}
-       </div>
-       </div>
+      <div className='text-white py-8'>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 0.5, }}
+          className='container flex justify-between items-center'>
+          <div>
+            <img src={img} alt='' className='max-w-[100px] invert' />
+          </div>
+          {/* Menu Section */}
+          <div className='hidden md:block'>
+            <ul className='flex items-center relative gap-4 z-40'>
+              {
+                NavbarMenu.map((item) => {
+                  return <li>
+                    <UpdateFollower
+                      mouseOptions={{
+                        backgroundColor: "white",
+                        zIndex: 10,
+                        followSpeed: 1.5,
+                        scale: 5,
+                        mixBlendMode: "difference",
+                      }}
+                    >
+                      <a href={item.link} className='text-base inline-block font-semibold py-2 px-3 uppercase'>{item.title}</a>
+                    </UpdateFollower>
+                  </li>
+                })}
+                <UpdateFollower
+                   mouseOptions={{
+                        backgroundColor: "white",
+                        zIndex: 10,
+                        followSpeed: 1.5,
+                        scale: 5,
+                        mixBlendMode: "difference",
+                      }}
+                >
+              <button className='ps-14 text-xl'>
+                <FaRegUser />
+              </button>
+              </UpdateFollower>
+            </ul>
+          </div>
+          {/* Menu Section */}
+          {/* Humbermenu Icon */}
+          <div className='md:hidden'>
+            <MdMenu className='text-4xl' />
+          </div>
+          {/* Humbermenu Icon */}
+        </motion.div>
+      </div>
     </>
   )
 }
